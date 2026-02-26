febcfe80f47d1a73bd08b5523ff028b4090e49e4
Paramvir Singh Thind <paramvirsingh2540@gmail.com>
2026-02-26 00:27:46 -0800

    updated Cart.action.ts

--- actions/customer/ProductAndStore/Cart.Action.ts
+++ actions/customer/ProductAndStore/Cart.Action.ts
@@ -1,72 +1,69 @@
-"use server"
+"use server";
 
 import CartModel from "@/db/models/customer/cart.model";
-import "@/db/models/store/products.model"
-import { getUser } from "../User.action";
+import "@/db/models/store/products.model";
 import { dbConnect } from "@/db/dbConnect";
 import mongoose from "mongoose";
 import { revalidatePath } from "next/cache";
+import { getCustomerDataAction } from "../User.action";
 
 export const AddtoCart = async (ItemId: string) => {
-
-  const user = await getUser();
+  const customerDataresponse = await getCustomerDataAction();
+  const user = customerDataresponse.customerData;
   if (!user) return null;
-  
+
   if (!user.associatedStoreId) throw new Error("No store assigned");
-  
+
   try {
-      await dbConnect();
-      const productObjectId = new mongoose.Types.ObjectId(ItemId);
-      const storeObjectId = new mongoose.Types.ObjectId(
-        user.associatedStoreId
-      );
-    
-      const existingCart = await CartModel.findOne({
+    await dbConnect();
+    const productObjectId = new mongoose.Types.ObjectId(ItemId);
+    const storeObjectId = new mongoose.Types.ObjectId(user.associatedStoreId);
+
+    const existingCart = await CartModel.findOne({
+      customerId: user._id,
+    });
+
+    if (!existingCart) {
+      await CartModel.create({
         customerId: user._id,
+        items: [
+          {
+            productId: productObjectId,
+            storeId: storeObjectId,
+            quantity: 1,
+          },
+        ],
       });
-    
-      if (!existingCart) {
-         await CartModel.create({
-          customerId: user._id,
-          items: [
-            {
-              productId: productObjectId,
-              storeId: storeObjectId,
-              quantity: 1,
-            },
-          ],
-        });
-        return { success: true };
-      }
-    
-      const itemIndex = existingCart.items.findIndex(
-        (item) => item.productId.toString() === ItemId
-      );
-    
-      if (itemIndex > -1) {
-        if (existingCart.items[itemIndex].quantity < 99) {
-          existingCart.items[itemIndex].quantity += 1;
-        }
-      } else {
-        existingCart.items.push({
-          productId: productObjectId,
-          storeId: storeObjectId,
-          quantity: 1,
-        });
-      }
-    
-      await existingCart.save();
       return { success: true };
+    }
+
+    const itemIndex = existingCart.items.findIndex(
+      (item) => item.productId.toString() === ItemId,
+    );
+
+    if (itemIndex > -1) {
+      if (existingCart.items[itemIndex].quantity < 99) {
+        existingCart.items[itemIndex].quantity += 1;
+      }
+    } else {
+      existingCart.items.push({
+        productId: productObjectId,
+        storeId: storeObjectId,
+        quantity: 1,
+      });
+    }
+
+    await existingCart.save();
+    return { success: true };
   } catch (error) {
-    console.log(error)
-    throw error
+    console.log(error);
+    throw error;
   }
 };
 
 export const IncrementItem = async (formData: FormData) => {
-  await dbConnect();
-
-  const user = await getUser();
+  const customerDataresponse = await getCustomerDataAction();
+  const user = customerDataresponse.customerData;
   if (!user) return;
 
   const ItemId = formData.get("productId");
@@ -74,15 +71,16 @@ export const IncrementItem = async (formData: FormData) => {
   if (!ItemId || typeof ItemId !== "string") {
     throw new Error("Invalid productId");
   }
-
+  await dbConnect();
   const cart = await CartModel.findOne({
     customerId: user._id,
   });
 
-  if (!cart) return;getCart
+  if (!cart) return;
+  getCart;
 
   const index = cart.items.findIndex(
-    (item) => item.productId.toString() === ItemId
+    (item) => item.productId.toString() === ItemId,
   );
 
   if (index === -1) return;
@@ -96,11 +94,9 @@ export const IncrementItem = async (formData: FormData) => {
   revalidatePath("/customer/cart");
 };
 
-
 export const DecrementItem = async (formData: FormData) => {
-  await dbConnect();
-
-  const user = await getUser();
+  const customerDataresponse = await getCustomerDataAction();
+  const user = customerDataresponse.customerData;
   if (!user) return;
 
   const ItemId = formData.get("productId");
@@ -108,12 +104,12 @@ export const DecrementItem = async (formData: FormData) => {
   if (!ItemId || typeof ItemId !== "string") {
     throw new Error("Invalid productId");
   }
-
+  await dbConnect();
   const cart = await CartModel.findOne({ customerId: user._id });
   if (!cart) return;
 
   const index = cart.items.findIndex(
-    (item) => item.productId.toString() === ItemId
+    (item) => item.productId.toString() === ItemId,
   );
 
   if (index === -1) return;
@@ -125,13 +121,12 @@ export const DecrementItem = async (formData: FormData) => {
   }
 
   await cart.save();
-  revalidatePath("/customer/cart")
+  revalidatePath("/customer/cart");
 };
 
-export const RemoveItem = async (formData: FormData)=>{
-  await dbConnect();
-
-  const user = await getUser();
+export const RemoveItem = async (formData: FormData) => {
+  const customerDataresponse = await getCustomerDataAction();
+  const user = customerDataresponse.customerData;
   if (!user) return;
 
   const ItemId = formData.get("productId");
@@ -139,12 +134,12 @@ export const RemoveItem = async (formData: FormData)=>{
   if (!ItemId || typeof ItemId !== "string") {
     throw new Error("Invalid productId");
   }
-
+  await dbConnect();
   const cart = await CartModel.findOne({ customerId: user._id });
   if (!cart) return;
 
   const index = cart.items.findIndex(
-    (item) => item.productId.toString() === ItemId
+    (item) => item.productId.toString() === ItemId,
   );
 
   if (index === -1) return;
@@ -153,19 +148,19 @@ export const RemoveItem = async (formData: FormData)=>{
     cart.items.splice(index, 1);
   }
   await cart.save();
-  revalidatePath("/customer/cart")
-}
+  revalidatePath("/customer/cart");
+};
 
 export const getCart = async () => {
+  const customerDataresponse = await getCustomerDataAction();
+  const user = customerDataresponse.customerData;
+  if (!user) return;
+  
   await dbConnect();
-
-  const user = await getUser();
-  if (!user) return null;
-
   try {
     const foundCart = await CartModel.findOne({
       customerId: user._id,
-    }).populate("items.productId");  
+    }).populate("items.productId");
 
     if (!foundCart) return null;
     return foundCart.items;
