7a57e148d166bbfd30800911a8d333358362aa0b
Paramvir Singh Thind <paramvirsingh2540@gmail.com>
2026-05-12 17:25:39 -0700

    updates

--- app/cashier/(cashier)/customer/orders/page.tsx
+++ app/cashier/(cashier)/customer/orders/page.tsx
@@ -1,11 +1,13 @@
 import CashierSidebar from "@/components/cashier/CashierSlidebar";
 import CustomerOrders from "@/components/shared/users/orders/CustomerOrders";
+import { notFound } from "next/navigation";
 
 const page = () => {
+  notFound();
   return (
     <>
-      <CashierSidebar />
-      <CustomerOrders allOrders={true} />
+      {/* <CashierSidebar />
+      <CustomerOrders allOrders={true} /> */}
     </>
   );
 };
