e1e36e28b9c8b8bdf5767fff0ed77c153aaac929
Manpreet.Singh18 <manpreet.singh18@student.ufv.ca>
2026-06-17 19:05:18 -0700

    Update SearchpageLoader.tsx

--- components/customer/search/SearchpageLoader.tsx
+++ components/customer/search/SearchpageLoader.tsx
@@ -22,7 +22,7 @@ export default async function SearchPageLoader() {
 
   return (
     <Suspense>
-      <Navbar />
+      {/* <Navbar /> */}
       <SearchResultsClient
         storeId={storeId}
         searchAction={searchProducts}
