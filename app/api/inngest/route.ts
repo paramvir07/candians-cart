6d8c2e5adb1960b7ece8aa0fdd4550d9f4087bb2
Shreyas Dutt <151562739+ShreyasDutt@users.noreply.github.com>
2026-06-30 19:39:40 -0700

    feat(route): add maxDuration constant for request handling

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -2,6 +2,7 @@ import { generateGeminiImage, helloWorld } from "@/lib/inngest/functions/ImageGe
 import { inngestClient } from "@/lib/inngest/inngest";
 import { serve } from "inngest/next";
 
+export const maxDuration = 300;
 
 export const { GET, POST, PUT } = serve({
   client: inngestClient,
