884bf1af7fdd9d9bba64d485dd2f654de980daad
Shreyas Dutt <151562739+ShreyasDutt@users.noreply.github.com>
2026-07-01 18:42:23 -0700

    feat(profile): refactor EditProfilePage to use Suspense and add loading skeletons

---
+++ components/customer/profile/EditProfileLoader.tsx
@@ -0,0 +1,30 @@
+import EditProfileForm from "@/components/customer/profile/EditProfileForm";
+import { dbConnect } from "@/db/dbConnect";
+import Customer from "@/db/models/customer/customer.model";
+import { auth } from "@/lib/auth/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+export async function EditProfileLoader() {
+  const session = await auth.api.getSession({ headers: await headers() });
+  if (!session) redirect("/customer/login");
+
+  await dbConnect();
+  const customerProfile = await Customer.findOne({
+    userId: session.user.id,
+  }).lean();
+
+  if (!customerProfile) redirect("/customer");
+
+  const mergedUserData = {
+    name: customerProfile.name || "",
+    email: customerProfile.email || "",
+    address: customerProfile.address || "",
+    city: customerProfile.city || "",
+    province: customerProfile.province || "",
+    postalCode: customerProfile.postalCode ?? "",
+    mobile: customerProfile.mobile || "",
+  };
+
+  return <EditProfileForm user={mergedUserData} />;
+}
